c80b4d0230917f0471d7484bada4dfd29907c141
klucas914 <kristinardell@yahoo.com>
2016-03-12 10:54:31 -0500

    Update prompts.js

--- scripts/prompts.js
+++ scripts/prompts.js
@@ -2,28 +2,28 @@
 module.prompts = {  
   prompt1 : {
     track_id : 'track1',
-    name : 'Finding Grace',
+    name : 'From Old to New',
     status : 'published',
-    scripture : 'John 10:4',
-    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    scripture : '2 Corinthians 5:17',
+    intro : 'When we follow Jesus, He transforms us--our habits, our actions, our desires. He makes us new. In the coming weeks we will embark on our own journeys of transformation as we encounter stories of Jesus meeting his followers and calling them to new life. Today, we reflect on how we have seen God transform things in the past and pray into how He is transforming us in the present!',
     group_id : 1
   },
   
   prompt2 : {
     track_id : 'track1',
-    name : 'Simple Prayer',
+    name : 'From Death to Life',
     status : 'published',
-    scripture : 'John 10:6',
-    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    scripture : 'Mark 5:21-43',
+    intro : 'In our story today, Jesus raises a dead girl from the dead and restores a chronically sick woman to health. In the same way, Jesus wants to bring the dead places in our hearts and lives back to life and health. How might Jesus be calling you back to life today?',
     group_id : 1
   },
 
   prompt3 : {
     track_id : 'track1',
-    name : 'Conflict Resolved',
+    name : 'From Kingdom of Self to Kingdom of God',
     status : 'published',
-    scripture : 'John 10:7',
-    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    scripture : 'Mark 1:14-20',
+    intro : 'Jesus calls his followers to brand new kinds of lives. They leave their jobs, their homes, and their families in order to follow him into a God-centered life. How might God be calling you to leave behind your own kingdoms in exchange for his?',
     group_id : 1
   }
 };
