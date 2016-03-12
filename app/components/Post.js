f6a1bfe1f4ed481bca3ea43639c5cf26fc96428f
Dustin Reed <dmreedy1@gmail.com>
2016-03-12 17:48:19 -0600

    Posts and Activities in Feed

--- app/components/Post.js
+++ app/components/Post.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 
 class Post extends React.Component {
   render() {
+  	var post = this.props.post;
+  	var activity = this.props.activity;
     return (
   		<div className="feed response">
 			<img src="images/images.jpg" className="user-img" alt="" />
-			<p className="user-name">Kristin <br />
+			<p className="user-name">{post.user_id} <br />
 			<span className="time-since-posted">5 mins ago</span></p>
-			<p className="activity-label"><a className="activity-name">The Jesus Who Sees</a></p>
-			<p className="response-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit tenetur, laborum ratione voluptatibus dolore, aliquam vitae repellendus eveniet odit deserunt debitis qui quo rerum totam hic, mollitia id tempore sit!</p>
+			<p className="activity-label"><a className="activity-name">{activity.name}</a></p>
+			<p className="response-text">{post.value}</p>
 			<a href="#" className="options" data-toggle="modal" data-target="#optionsModal"><i className="fa fa-ellipsis-v"></i></a>
 		</div>
     );
