40587e25b2192e6cabff064206a06a70f7fb4eab
Chance Smith <chance@sodiumhalogen.com>
2016-03-12 10:54:28 -0600

    adds post component

---
+++ app/components/Post.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+  		<div className="feed response">
+			<img src="images/images.jpg" className="user-img" alt="">
+			<p className="user-name">Kristin <br>
+			<span className="time-since-posted">5 mins ago</span></p>
+			<p className="prompt-label"><a className="prompt-name">The Jesus Who Sees</a></p>
+			<p className="response-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit tenetur, laborum ratione voluptatibus dolore, aliquam vitae repellendus eveniet odit deserunt debitis qui quo rerum totam hic, mollitia id tempore sit!</p>
+			<a href="#" className="options" data-toggle="modal" data-target="#optionsModal"><i className="fa fa-ellipsis-v"></i></a>
+		</div>
+    );
+  }
+}
+
+export default Post;
