3f378ed579e5edbdae0067780af07ddbbce88380
klucas914 <kristinardell@yahoo.com>
2016-03-12 11:00:44 -0500

    Update reflections.js

--- scripts/reflections.js
+++ scripts/reflections.js
@@ -2,7 +2,7 @@
 module.reflections = {  
   reflections1 : {
     prompt_id : 'prompt1',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    value : 'Reflect upon ways you have seen God transform things in the past. Where have you seen him make old things new again? Where have you seen him take things that were dead and bring them back to life? Think about examples you might find in nature, in history, or in your own world. Enter your reflections into the Old/New list below.'
     input_type : 'text'
   },
   
