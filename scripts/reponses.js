cb52a019ca830ee7440012ebf584f433e93e71ab
klucas914 <kristinardell@yahoo.com>
2016-03-12 12:01:06 -0500

    Update reponses.js

--- scripts/reponses.js
+++ scripts/reponses.js
@@ -2,16 +2,26 @@
 module.responses = {  
   responses1 : {
     prompt_id : 'prompt1',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+    value : 'Set some prayer goals for the next few weeks. What are you asking God to transform in you? List 2-3 specific things you will be praying about.'
   },
   
   responses2 : {
     prompt_id : 'prompt2',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+    value : 'Option 1: Share a picture of something in nature that is grasping at life, just like the woman with the chronic sickness. Create your own caption to describe what is happening.'
   },
 
   responses3 : {
+    prompt_id : 'prompt2',
+    value : 'Option 2: Find a spot (in nature, if possible) to take a prayer walk. As you walk, have a conversation with God about the specific ways he would like to bring you back to life and what you can do to respond. If specific things come to mind, list them as a journal entry.'
+  },
+  
+  responses4 : {
     prompt_id : 'prompt3',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
-  }
+    value : 'Option 1: Upload a photo of a clock or create a photo collage of different types of clocks. Pray and ask God to help you fill in the rest of this sentence and add it as a caption: The time has come for...'
+  },
+  
+  responses5 : {
+    prompt_id : 'prompt3',
+    value : 'Option 2: '
+  },
 };
