f7b429ce8e98f6814f6bc24c26ac6be00538ca3f
Dustin Reed <dmreedy1@gmail.com>
2016-03-12 09:46:52 -0600

    Nav Bar Component

--- app/App.js
+++ app/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   render() {
     return (
       <div>
+      	<NavBar />
+      	<Header />
         <h1>
-          Welcome to the react starter Test.
+          Welcome to Formed App.
         </h1>
       </div>
     );
---
+++ app/components/NavBar.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      		<nav className="navbar navbar-default">
+				<div className="container-fluid">
+					<div className="navbar-header">
+						<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+							<i className="fa fa-ellipsis-v"></i>
+						</button>
+						<a className="navbar-brand" href="#">Formed</a>
+					</div>
+					<div id="navbar" className="navbar-collapse collapse">
+						<ul className="nav navbar-nav">
+							<li className="active"><a href="#">Home</a></li>
+							<li><a href="#">Live</a></li>
+						</ul>
+						<ul className="nav navbar-nav navbar-right">
+							<li><a href="#">Signup</a></li>
+							<li><a href="#">Login</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+    );
+  }
+}
+
+export default NavBar;
