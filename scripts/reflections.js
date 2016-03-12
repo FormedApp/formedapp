33f7ed9fdef46467b3217f2ed5c092db50f4e108
klucas914 <kristinardell@yahoo.com>
2016-03-12 11:42:51 -0500

    Update reflections.js

--- scripts/reflections.js
+++ scripts/reflections.js
@@ -2,19 +2,25 @@
 module.reflections = {  
   reflections1 : {
     prompt_id : 'prompt1',
-    value : 'Reflect upon ways you have seen God transform things in the past. Where have you seen him make old things new again? Where have you seen him take things that were dead and bring them back to life? Think about examples you might find in nature, in history, or in your own world. Enter your reflections into the Old/New list below.'
+    value : 'Reflect upon ways you have seen God transform things in the past. Where have you seen him make old things new again? Where have you seen him take things that were dead and bring them back to life? Think about examples you might find in nature, in history, or in your own world. Enter your reflections into the list below.'
     input_type : 'text'
   },
   
   reflections2 : {
     prompt_id : 'prompt2',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    value : 'Which character from this story do you relate to the most today? (Jairus, the little girl, the sick woman, the crowd, Jesus) ',
     input_type : 'text'
   },
 
   reflections3 : {
-    prompt_id : 'prompt3',
-    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    prompt_id : 'prompt2',
+    value : 'What is one word to describe this character that also describes you today?',
     input_type : 'text'
-  }
+  },
+  
+ reflections4 : {
+    prompt_id : 'prompt2',
+    value : 'What is one prayer this character might pray that you might also pray today?',
+    input_type : 'text'
+  }, 
 };
