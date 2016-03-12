d107d5fdabc681080bbbcde62ec965aefd963593
Chance Smith <chance@sodiumhalogen.com>
2016-03-12 01:02:34 -0600

    begins data models for tracks

---
+++ scripts/prompts.js
@@ -0,0 +1,26 @@
+// prompts
+module.prompts = {  
+  prompt1 : {
+    track_id : 'track1',
+    name : 'Finding Grace',
+    status : 'published',
+    scripture : 'John 10:4',
+    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+  
+  prompt2 : {
+    track_id : 'track1',
+    name : 'Simple Prayer',
+    status : 'published',
+    scripture : 'John 10:6',
+    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+
+  prompt3 : {
+    track_id : 'track1',
+    name : 'Conflict Resolved',
+    status : 'published',
+    scripture : 'John 10:7',
+    intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  }
+};
---
+++ scripts/reflections.js
@@ -0,0 +1,20 @@
+// reflections
+module.reflections = {  
+  reflections1 : {
+    track_id : 'prompt1',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    input_type : 'text'
+  },
+  
+  reflections2 : {
+    track_id : 'prompt2',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    input_type : 'text'
+  },
+
+  reflections3 : {
+    track_id : 'prompt3',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
+    input_type : 'text'
+  }
+};
