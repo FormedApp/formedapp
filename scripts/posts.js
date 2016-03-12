b3a745f26b6bbc74b9db4e4c4d49c3a6e382c5d9
Chance Smith <chance@sodiumhalogen.com>
2016-03-12 09:14:09 -0600

    adds codekit ignore, creates posts model

---
+++ scripts/posts.js
@@ -0,0 +1,50 @@
+// posts
+module.posts = {  
+  post1 : {
+    user_id : 'user1',
+    prompt_id : 'prompt1',
+    private : false,
+    type : 'response',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+  
+  post2 : {
+    user_id : 'user2',
+    prompt_id : 'prompt1',
+    private : false,
+    type : 'response',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+
+  post3 : {
+    user_id : 'user3',
+    prompt_id : 'prompt1',
+    private : true,
+    type : 'response',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+
+  post4 : {
+    user_id : 'user1',
+    prompt_id : 'prompt1',
+    private : false,
+    type : 'reflection',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+
+  post5 : {
+    user_id : 'user2',
+    prompt_id : 'prompt1',
+    private : false,
+    type : 'reflection',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  },
+
+  post6 : {
+    user_id : 'user3',
+    prompt_id : 'prompt1',
+    private : false,
+    type : 'reflection',
+    value : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?'
+  }
+};
--- scripts/prompts.js
+++ scripts/prompts.js
@@ -6,8 +6,7 @@ module.prompts = {
     status : 'published',
     scripture : 'John 10:4',
     intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
-    group_id : 1,
-    track_id : 'track1'
+    group_id : 1
   },
   
   prompt2 : {
@@ -16,8 +15,7 @@ module.prompts = {
     status : 'published',
     scripture : 'John 10:6',
     intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
-    group_id : 1,
-    track_id : 'track1'
+    group_id : 1
   },
 
   prompt3 : {
@@ -26,7 +24,6 @@ module.prompts = {
     status : 'published',
     scripture : 'John 10:7',
     intro : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolore explicabo quis, maxime vitae. Obcaecati, assumenda nam fugiat, adipisci quo odio, animi nisi aspernatur nulla excepturi vero at cumque voluptates?',
-    group_id : 1,
-    track_id : 'track1'
+    group_id : 1
   }
 };
