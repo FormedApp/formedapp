5e7788ff9c5eb50ffe7eebced60136008b2ce87d
Dustin Reed <dmreedy1@gmail.com>
2016-03-12 11:09:59 -0600

    CSS Webpack Configured

--- app/App.js
+++ app/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+var css = require("./css/style.scss");
 
 class App extends Component {
   render() {
@@ -10,7 +11,7 @@ class App extends Component {
       	<NavBar />
       	<Header />
         <h1>
-          Welcome to Formed App.
+          Welcome to Formed App Test 4.
         </h1>
       </div>
     );
