d7c745bfa111c1a002777d680aa012f97bf6832f
Chance Smith <chance@sodiumhalogen.com>
2016-03-13 14:21:14 -0500

    styles activities

--- app/components/Activity.js
+++ app/components/Activity.js
@@ -15,21 +15,19 @@ class Activity extends React.Component {
 	renderData(key) {
 		let data = this.props.data[key];
 		return (
-			<a href="#" onClick={this.handleClick.bind(this, 'receive')}>
+			<div onClick={this.handleClick.bind(this, 'receive')}>
 		  		<div className="track activity">
 					<h1 className="activity-name">{data.name}</h1>
 					<p className="track-name">{data.track_name}</p>
 				</div>
-			</a>
+			</div>
 		)
 	}
 
 	render() {
 		return(
-			<div>
-				<ul className="">
-						{Object.keys(this.props.data).map(this.renderData.bind(this))}
-				</ul>
+			<div className="container">
+				{Object.keys(this.props.data).map(this.renderData.bind(this))}
 			</div>
 		);
 	}
