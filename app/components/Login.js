95f6e3a8b4af5a6000ea703ba73ed824b786e640
Chance Smith <chance@sodiumhalogen.com>
2016-03-23 11:01:00 -0500

    build Login comp futher - testing

--- app/components/Login.js
+++ app/components/Login.js
@@ -13,10 +13,12 @@ class Login extends React.Component {
 	}
   render() {
     return (
-			<form className="store-selector" onSubmit={this.goToStore}>
-				<h2>Please Enter A Store</h2>				
-				<input type="submit" />
-			</form>
+		<form className="login">
+			<h2>Login</h2>				
+			<input type="text" placeholder="Email"/>
+			<input type="password" placeholder="Password"/>
+			<input type="submit" />
+		</form>
     );
   }
 }
