efc6e278b79026b5ccd70423593a8cdbe39b8e49
Chance Smith <chance@sodiumhalogen.com>
2016-03-26 14:13:11 -0500

    adds login comp

--- app/App.js
+++ app/App.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Activity from "./components/Activity";
-import Receive from "./components/Receive";
-import Response from "./components/Response";
-import Success from "./components/Success";
-import Profile from "./components/Profile";
-import Feed from "./components/Feed";
 import CSS from "./css/style.scss";
 import autobind from 'autobind-decorator';
 import reactMixin from 'react-mixin';
 import Catalyst from 'react-catalyst';
 
-/* Routes
-1. Community
-2. Activities
-3. Journal
-4. Profile
-5. Leader
+// header & footer
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+/* 
+// Routes
 */
+// accounts comps
+import Login from "./components/Login";
+// import ResetPass from "./components/ResetPass";
+// import Signup from "./components/Signup";
+// pages
+import Feed from "./components/Feed";
+import Activity from "./components/Activity";
+import Receive from "./components/Receive";
+import Response from "./components/Response";
+import Success from "./components/Success";
+import Profile from "./components/Profile";
+
 
 class App extends React.Component {
 	
@@ -29,7 +33,7 @@ class App extends React.Component {
 			posts : require('./scripts/posts'),
 			activities : require('./scripts/activities'),
 			journal : require('./scripts/journal'),
-			route: 'community'
+			route: 'login' // defult route on load
 		};
 		this.handleNavChange = this.handleNavChange.bind(this);
 	}
@@ -45,7 +49,9 @@ class App extends React.Component {
 		let content;
 
 
-		if(route === 'community') {
+		if(route === 'login') {
+			content = <Login navChangeCallback={this.handleNavChange} {...this.props}/>;
+		} else if(route === 'community') {
 			content = <Feed data={this.state.posts} {...this.props}/>;
 		} else if(route === 'activity') {
 			content = <Activity data={this.state.activities} navChangeCallback={this.handleNavChange} {...this.props}/>;
@@ -60,8 +66,8 @@ class App extends React.Component {
 		} else if(route === 'profile') {
 			content = <Profile {...this.props}/>;
 		} else {
-			//back to community
-			content = <Feed data={this.state.posts} {...this.props}/>;
+			//back to login
+			content = <Login navChangeCallback={this.handleNavChange} {...this.props}/>;
 		}
 
 		return (
--- app/components/Login.js
+++ app/components/Login.js
@@ -5,22 +5,40 @@ import autobind from 'autobind-decorator';
 
 @autobind
 class Login extends React.Component {
+	handleClick(newRoute) {
+		if (newRoute) {
+			this.props.navChangeCallback(newRoute);
+		}
+	}
 	goToStore(event) {
 		event.preventDefault();
 		// get the data from the input
 		this.history.pushState(null, '/feed');
 		// transition from <StorPicker/> to <App/>
 	}
-  render() {
-    return (
-		<form className="login">
-			<h2>Login</h2>				
-			<input type="text" placeholder="Email"/>
-			<input type="password" placeholder="Password"/>
-			<input type="submit" />
-		</form>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-8 col-xs-offset-2">
+						<form className="login">
+							<h2>Login</h2>
+							<fieldset className="form-group">
+								<input type="text" className="form-control" placeholder="Email"/>
+							</fieldset>
+							<fieldset className="form-group">
+								<input type="password" className="form-control" placeholder="Password"/>
+							</fieldset>
+							<fieldset className="form-group">
+								<input type="submit" className="btn btn-primary btn-block" onClick={this.handleClick.bind(this, 'community')} />
+							</fieldset>
+							<p className="text-center"><a href="#" onClick={this.handleClick.bind(this, 'signup')}>Signup?</a></p>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 reactMixin.onClass(Login, History);
